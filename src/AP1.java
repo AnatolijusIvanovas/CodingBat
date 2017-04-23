b64c3a9a2d7a49ec647b45811dcd995fc9e5bd3b
Anatolijus <join.eu.pn@gmail.com>
2017-04-23 23:41:55 +0300

    AP1 Solutions

--- src/AP1.java
+++ src/AP1.java
@@ -349,8 +349,17 @@ public class AP1 {
     commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
     commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3*/
     public int commonTwo(String[] a, String[] b) {
-        int out = 0;
-        return out;
+        int count = 0;
+        String str = "";
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[j].equals(b[i]) && !str.contains(a[j])) {
+                    str += a[j];
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
 }
