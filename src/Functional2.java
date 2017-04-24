f0c743917a5a666ba7b627ce3aaa4f5d1fd7957a
Anatolijus <join.eu.pn@gmail.com>
2017-04-24 21:35:51 +0300

    Functional Solutions

--- src/Functional2.java
+++ src/Functional2.java
@@ -72,8 +72,9 @@ public class Functional2 {
     no34(["a", "bb", "ccc", "dddd"]) → ["a", "bb"]
     no34(["ccc", "dddd", "apple"]) → ["apple"]*/
     public List<String> no34(List<String> strings) {
-
-        return strings;
+        return strings.stream()
+                .filter(s -> s.length() != 3 && s.length() != 4)
+                .collect(Collectors.toList());
     }
 
     /*Functional-2 > noYY
